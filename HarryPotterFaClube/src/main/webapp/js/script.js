c57b6ae80256fbbc501c535a27eb3c7110dfb030
CarlosLaurindo1 <laurindo.ale@gmail.com>
2023-05-31 21:04:28 -0300

    change: adicionando expressões regulares para o fã clube.

--- HarryPotterFaClube/src/main/webapp/js/script.js
+++ HarryPotterFaClube/src/main/webapp/js/script.js
@@ -2,3 +2,13 @@ function validaAceite(value) {
     var enviarButton = document.getElementsByName("enviar")[0];
     enviarButton.disabled = !value;
 }
+
+function validaCamposInscricao{
+    var nome = document.inscricao.txtnome.value;
+    var expRegNome = new RegExp("^[A-zÀ-ü]{3,}([ ]{1}[A-zÀ-ü]{2,})+$");
+
+    if(!expRegNome.test(nome)){
+        alert("Preencha o campo Nome corretamente.");
+        document.inscricao.txtnome.
+    }
+}}
