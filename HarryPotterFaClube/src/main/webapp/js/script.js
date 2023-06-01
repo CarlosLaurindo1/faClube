9d22fb37de81fb7b23e57db10d1fff43cb57451b
CarlosLaurindo1 <laurindo.ale@gmail.com>
2023-06-01 19:57:10 -0300

    change: ajustadas as expressões regulares dos campos da página de inscrição

--- HarryPotterFaClube/src/main/webapp/js/script.js
+++ HarryPotterFaClube/src/main/webapp/js/script.js
@@ -3,12 +3,43 @@ function validaAceite(value) {
     enviarButton.disabled = !value;
 }
 
-function validaCamposInscricao{
-    var nome = document.inscricao.txtnome.value;
+function validaCamposInscricao(){
+    var nome = document.frmInscricao.txtnome.value;
     var expRegNome = new RegExp("^[A-zÀ-ü]{3,}([ ]{1}[A-zÀ-ü]{2,})+$");
 
-    if(!expRegNome.test(nome)){
+    if (!expRegNome.test(nome)) {
         alert("Preencha o campo Nome corretamente.");
-        document.inscricao.txtnome.
+        document.frmInscricao.txtnome.focus();
+        return false;
     }
-}}
+
+    var dataNascimento = document.frmInscricao.datanascimento.value;
+    var expRegNasc = new RegExp ("^[0-3]{1}[0-9]{1}[/]{1}[0-1]{1}[0-9]{1}[/]{1}[0-2]{1}[0-9]{3}$");
+
+    if(!expRegNasc.test(dataNascimento)){
+        alert("Preencha o campo Data de Nascimento corretamente");
+        //rever o regexp deste caso
+        document.frmInscricao.dataNascimento.focus();
+    }
+
+    var mail = document.frmInscricao.txtemail.value;
+    var expRegMail = new RegExp("^[a-z1-9]{4,}[@]{1}[a-z1-9]{2,}[.]{1}[a-z]{2,}$");
+
+    if (!expRegMail.test(mail)) {
+        alert("Preencha o campo E-mail corretamente.");
+        document.frmInscricao.txtemail.focus();
+        return false;
+    }
+
+    var fone = document.frmInscricao.txtfone.value;
+    var expRegFone = new RegExp("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$");
+
+    if (!expRegFone.test(fone)) {
+        alert("Preencha o campo Telefone corretamente.");
+        document.frmInscricao.txtfone.focus();
+        return false;
+    }
+
+   
+    return true;
+}
